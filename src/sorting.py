0fafb6db136e9c3bbc4344218d61c10738e93eea
r-nasc <89160389+r-nasc@users.noreply.github.com>
2023-11-19 15:53:10 -0400

    Improved docstrings

--- src/sorting.py
+++ src/sorting.py
@@ -3,7 +3,14 @@ import random
 
 
 def shell_sort(arr: list) -> list:
-    """Sorts an array using the Shell sort algorithm."""
+    """Sorts an array using the Shell sort algorithm.
+
+    Time Complexity: O(n^3/2) avg and worst case. Memory: O(1)
+
+    The method calculates a gap to be used (3x+1 in this example) and gap-sort
+    the array. The gap is reduced until it reaches 1, which is the same as an
+    insertion sort on a partially sorted array.
+    """
     count = len(arr)
     gap = 1
 
@@ -23,7 +30,16 @@ def shell_sort(arr: list) -> list:
 
 
 def insertion_sort(arr: list) -> list:
-    """Sorts an array using the Insertion sort algorithm."""
+    """Sorts an array using the Insertion sort algorithm.
+
+    Time complexity: O(n^2) avg and worst case. Memory: O(1)
+
+    The method uses a for loop to iterate over each element in the list, starting
+    from the second element (i==1). The while loop is used to iterate over the
+    sorted list and find the correct position of the current element by shifting
+    all greater elements to the right and inserting the current element after a
+    smaller element is found.
+    """
     for i in range(1, len(arr)):
         key_item = arr[i]
         j = i - 1
@@ -35,7 +51,14 @@ def insertion_sort(arr: list) -> list:
 
 
 def selection_sort(arr: list) -> list:
-    """Sorts an array using the Selection sort algorithm."""
+    """Sorts an array using the Selection sort algorithm.
+
+    Time complexity: O(n^2) avg and worst case. Memory: O(1)
+
+    The method uses a for loop to iterate over each element in the list. The
+    inner for loop is used to find the smallest element in the unsorted part of
+    the list and swap it with the first element of the unsorted part of the list.
+    """
     n = len(arr)
     for i in range(n):
         min_idx = i
@@ -47,8 +70,16 @@ def selection_sort(arr: list) -> list:
 
 
 def shuffle_sort(arr: list) -> list:
-    """Sorts an array using the Shuffle sort algorithm."""
+    """Sorts an array using the Shuffle sort algorithm.
+
+    Time complexity: O(n) avg and worst case. Memory: O(1)
+
+    The method uses a for loop to iterate over each element in the list and swap
+    it with a random element in the list between the starting and current element.
+    """
     for i in range(1, len(arr)):
         # pick an element in x[:i] with which to exchange x[i]
         j = random.randint(0, i)
         arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
